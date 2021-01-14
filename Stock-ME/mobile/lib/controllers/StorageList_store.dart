35281bd5ada1fb8904f62d7da8118c511cf5ca44
FredericoPM <52333585+FredericoPM@users.noreply.github.com>
2021-01-14 16:13:38 -0300

    mobx testes

---
+++ Stock-ME/mobile/lib/controllers/StorageList_store.dart
@@ -0,0 +1,9 @@
+import 'package:mobx/mobx.dart';
+
+part 'StorageList_store.g.dart';
+
+class StorageListStore extends _StorageListStore with _$StorageListStore {}
+
+abstract class _StorageListStore with Store {
+  
+}
