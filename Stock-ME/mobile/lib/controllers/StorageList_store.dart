f0434fc57c20112026f7fd0b1389aa3e9e207d28
FredericoPM <52333585+FredericoPM@users.noreply.github.com>
2021-01-15 11:07:28 -0300

    testes

--- Stock-ME/mobile/lib/controllers/StorageList_store.dart
+++ Stock-ME/mobile/lib/controllers/StorageList_store.dart
@@ -1,9 +1,13 @@
+import 'package:mobile/models/Products.dart';
 import 'package:mobx/mobx.dart';
 
 part 'StorageList_store.g.dart';
 
-class StorageListStore extends _StorageListStore with _$StorageListStore {}
+class StorageListStore extends _StorageListStore with _$StorageListStore {
+  StorageListStore(Products productsRep) : super(productsRep);
+}
 
 abstract class _StorageListStore with Store {
-  
+  final Products productsRep;
+  _StorageListStore(this.productsRep);
 }
---
+++ Stock-ME/mobile/lib/models/Products.dart
@@ -0,0 +1,8 @@
+class Products{
+    String name;
+    double price;
+    Products(){
+      name = "teste";
+      price = 10.0;
+    }
+}
